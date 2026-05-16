e01ab7fe06b3d19422e99882e95d01621ae878a8
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-05-16 17:43:48 -0400

    Refactor

--- compiler.js
+++ compiler.js
@@ -4,9 +4,12 @@ module.exports = { compile: compile };
 function compile(ast) {
 
 	return `
-		if (keystates["Mouse"]) {
-			ctx.fillStyle = "#000";
-			ctx.fillText("mouse is down!", 0, 20);
+		function process() {
+
+			if (INTERNAL_keystates["Mouse"]) {
+				INTERNAL_ctx.fillStyle = "#000";
+				INTERNAL_ctx.fillText("mouse is down!", 0, 20);
+			}
 		}
 	`;
 }
